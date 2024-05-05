518653a3c89c510490188ecd213245a2a1c03313
Krrio <kacperjozwik12@gmail.com>
2024-05-05 23:23:20 +0200

    final commit1

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 import Image from 'next/image';
 
 const Page = () => {
@@ -18,17 +17,16 @@ const Page = () => {
 
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <div className="bg-blue-500 rounded-lg h-[70vh] w-11/12 md:w-3/4 lg:w-1/2 flex flex-col justify-start items-center">
-        <span className="text-4xl md:text-6xl font-bold text-white mt-8 md:mt-16">Lot na Bali!</span>
+      <div className="bg-blue-500 rounded-lg h-[70vh] w-full md:w-3/4 lg:w-1/2 flex flex-col justify-start items-center p-4">
+        <span className="text-4xl md:text-6xl font-bold text-white mt-4 md:mt-8">Lot na Bali!</span>
         <Image src="/images/line.png" alt='line' width={112} height={112} className='ml-[50%] -translate-x-1/2 mt-2 md:mt-4' />
-        <Image src="/images/logo-plane.png" alt='airline' width={112} height={112} className='absolute left-1/2 -translate-x-[350%] md:translate-x-[-400%] mt-4 md:mt-8' />
-        <span className="text-lg md:text-xl font-semibold text-white mt-16 md:mt-32 text-center">
+        <span className="text-lg md:text-xl font-semibold text-white mt-4 md:mt-8 text-center">
           To jest brama, którą musisz otworzyć, aby spełnić swoje marzenie.
         </span>
-        <span className="text-md md:text-lg font-medium text-white mt-4 md:mt-8 text-center">
+        <span className="text-md md:text-lg font-medium text-white mt-2 md:mt-4 text-center">
           Wchodzisz?
         </span>
-        <div className="flex gap-4 mt-4 md:mt-8">
+        <div className="flex gap-4 mt-2 md:mt-4">
           <Button
             variant="default"
             className="bg-orange-500 text-white"
@@ -38,7 +36,7 @@ const Page = () => {
           </Button>
           <Button
             variant="outline"
-            className={cn("text-white border-white transition-transform duration-100")}
+            className="text-white border-white transition-transform duration-100"
             onMouseEnter={moveButton}
             style={{
               transform: `translate(${offset.x}px, ${offset.y}px)`
@@ -47,8 +45,9 @@ const Page = () => {
             Nie wchodzę :(
           </Button>
         </div>
-        <div className='mt-4 md:mt-8'>
+        <div className='mt-4 md:mt-8 flex flex-col items-center'>
           <span className='font-light text-sm md:text-base text-white'>Bilet do kupienia we własnym zakresie.</span>
+          <Image src="/images/logo-plane.png" alt='airline' width={56} height={56} />
         </div>
       </div>
       <div className="flex-grow"></div>
